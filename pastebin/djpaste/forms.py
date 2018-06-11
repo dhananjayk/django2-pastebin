fdc0788eda3be8bd717d029d1a815a9cd5a10496
Dhananjay Desktop <dhananjay1312@gmail.com>
2018-06-11 23:27:01 +0530

    Added new fields to Paste form
    Added the fields paste_name, paste_expiration, private_paste, syntax_highlighting

--- pastebin/djpaste/forms.py
+++ pastebin/djpaste/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
+SYNTAX_CHOICES = (
+    ('', ''),
+    ('css', 'CSS'),
+    ('javascript', 'JavaScript'),
+    ('python', 'Python'),
+)
+
 
 class PasteForm(forms.Form):
-    text = forms.CharField(label='New Paste',  widget=forms.Textarea(attrs={'rows': '10', 'class': 'form-control'}))
+    text = forms.CharField(label='New Paste', widget=forms.Textarea(attrs={'rows': '10', 'class': 'form-control'}))
+    paste_name = forms.CharField(label='Paste Name', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    paste_expiration = forms.DateField(required=False, widget=forms.DateInput(attrs={'class': 'form-control datepicker'}))
+    private_paste = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    syntax_highlighting = forms.ChoiceField(choices=SYNTAX_CHOICES, required=False, widget=forms.Select(attrs={'class': 'form-control'}))
